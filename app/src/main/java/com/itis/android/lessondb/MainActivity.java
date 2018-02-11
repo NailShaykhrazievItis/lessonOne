9d3539aeb3a4bcbc4b4ce32cdb8f016f071fbe12
Nail <shaykhrazievnail@gmail.com>
2018-02-11 19:41:41 +0300

    init

---
+++ app/src/main/java/com/itis/android/lessondb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.itis.android.lessondb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
